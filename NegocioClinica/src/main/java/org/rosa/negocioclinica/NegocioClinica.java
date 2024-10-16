121e06c57fbbc8f458bef419757d126648bdd2db
rosa <natsukiarr@gmail.com>
2024-10-15 20:17:20 -0700

    ADD base project

---
+++ NegocioClinica/src/main/java/org/rosa/negocioclinica/NegocioClinica.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.rosa.negocioclinica;
+
+/**
+ *
+ * @author natsu
+ */
+public class NegocioClinica {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
